f3c5411e38d4174f57f9b1e8a5f3fbc3b299ba92
Aleksandra <aleksandra.sles22@gmail.com>
2023-12-23 20:37:41 +0200

    Twice oblique trochoid cylindroid

--- main.js
+++ main.js
@@ -99,7 +99,8 @@ function CreateSurfaceData() {
           let x = c * u + v * (Math.sin(phi) + Math.tan(alpha) * Math.cos(phi) * Math.cos(theta))
           let y = v * Math.tan(alpha) * Math.sin(theta)
           let z = H + v * (Math.tan(alpha) * Math.cos(theta) - Math.cos(phi)); 
-
+          vertexList.push(x * 0.3, y * 0.3, z * 0.3);
+          
     }
   }
   return vertexList;
